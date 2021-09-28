41c8a50f5ecfb45d6165614ece9d9ad67cf32d2d
Maicol Andrés Garzón Fonseca <55245364+MikeGarzon@users.noreply.github.com>
2021-09-28 10:25:37 -0500

    Delete wal directory

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f3c187", "#121d1f" },
-	[SchemeSel] = { "#f3c187", "#DC3326" },
-	[SchemeOut] = { "#f3c187", "#BD9F2F" },
-};
